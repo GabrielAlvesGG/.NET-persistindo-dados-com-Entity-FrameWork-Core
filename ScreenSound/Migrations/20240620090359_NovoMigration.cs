efc628019b1f7db91a2a4a8441e9694f7ba238a3
Gabriel Alves <alves.gabriel7@gmail.com>
2024-06-20 06:51:02 -0300

    Implementando uma nova tabela na migration e também inserindo uma nova coluna após ter criado a tabela pelo migration

---
+++ ScreenSound/Migrations/20240620090359_NovoMigration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class NovoMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Artistas", new string[] { "Nome", "Bio", "FotoPerfil" }, new object[] {"Djavan",
+                "Djavan bio dele", "FotoLinda.png"});
+
+            migrationBuilder.InsertData("Artistas", new string[] { "Nome", "Bio", "FotoPerfil" }, new object[] {"Pitty",
+                "Cabelo azul", "FotoLindaPitty.png"});
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Artistas");
+        }
+    }
+}
